7bfe8f489be1454ec248dfdacb5dd1f465ced068
Anastasiia Shved <ananashw@gmail.com>
2023-10-23 21:11:01 +0700

    change script in task1

--- task1/script.js
+++ task1/script.js
@@ -1,10 +1,7 @@
-//кнопка показать окно
 let modalShow = document.querySelector("#modalShow");
-//кнопка скрыть окно
 let modalHide = document.querySelector("#modalHide");
 let modal = document.querySelector(".modal");
 
-//события
 modalShow.onclick = show;
 modalHide.onclick = hide;
 
